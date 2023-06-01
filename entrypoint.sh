5fd4b8137edb5446061ae6d027acb5cb46e7d798
liricooli <liricooli@gmail.com>
2023-06-01 18:40:50 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,17 +9,17 @@ npm --version
 echo "--- Cypress version"
 npx cypress --version
 
-echo "--- Env vars"
-chmod 777 scripts/read_envs.sh
-scripts/read_envs.sh
+# echo "--- Env vars"
+# chmod 777 scripts/read_envs.sh
+# scripts/read_envs.sh
 
 echo "--- Installing npm packages"
 npm install --save-dev
 npm install
 
-echo "--- Verifying Redefine"
-chmod 777 scripts/verify_redefine.sh
-scripts/verify_redefine.sh
+# echo "--- Verifying Redefine"
+# chmod 777 scripts/verify_redefine.sh
+# scripts/verify_redefine.sh
 
 echo "+++ Run Cypress tests"
 npm run test
