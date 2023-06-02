2a88ca6b28abf5cf28f81a7325d004e7521cb505
liricooli <liricooli@gmail.com>
2023-06-02 09:50:12 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,6 +32,9 @@ pip install ${BUILDKITE_BUILD_CHECKOUT_PATH}/redefine-0.14.16.post8-py3-none-man
 redefine config set environment=staging
 redefine config set redefine_address=dune-tf-staging.redefine.dev
 redefine config set stable_branch=master
+redefine config set source_branch=master
+redefine config set source_commit=c8f03c92960b51b73011d96dfa68a42ab8268bc0
+redefine config set suite_id=master
 redefine start --verbose --cypress --worker
 # npx cypress run
 # env
