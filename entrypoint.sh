0afc6a2c47619a13936e318d72b663005cba3555
liricooli <liricooli@gmail.com>
2023-06-02 08:45:14 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -24,6 +24,9 @@ echo "liron"
 # export REDEFINE_AUTH=e88afb65-65ef-4798-bc9d-db4c5342f7f3::163bf879-4060-4636-9fb6-49161e68095a
 # pip3 install -U redefine --index-url https://redefine.dev/pip/
 
+git status
+git branch -a
+
 pip install ${BUILDKITE_BUILD_CHECKOUT_PATH}/redefine-0.14.16.post8-py3-none-manylinux1_x86_64.whl
 redefine config set environment=staging
 redefine config set redefine_address=dune-tf-staging.redefine.dev
