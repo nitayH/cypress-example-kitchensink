6df00e45bbea4a6c11c52f7a37bcdc223f10bf37
liricooli <liricooli@gmail.com>
2023-06-02 08:04:08 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -16,6 +16,7 @@
 # echo "--- Installing npm packages"
 # npm install --save-dev
 # npm install
+echo "liron"
 npm install cypress --save-dev
 
 # echo "--- Verifying Redefine"
@@ -28,7 +29,6 @@ redefine config set environment=staging
 redefine config set redefine_address=dune-tf-staging.redefine.dev
 redefine config set stable_branch=master
 redefine start --verbose --cypress --worker
-echo "liron"
 # npx cypress run
 env
 cat /app/redefine/1.txt
