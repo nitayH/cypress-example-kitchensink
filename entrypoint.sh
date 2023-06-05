9beec5e32c873070aab5b6b624d44778b2259166
liricooli <liricooli@gmail.com>
2023-06-05 17:36:23 +0300

    WIP

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+# shellcheck disable=SC2155
 # echo "--- Node version"
 # node --version
 
@@ -30,10 +30,4 @@ redefine config set stable_branch=master
 redefine start --verbose --cypress --worker
 
 # The full spec list provided by redefine exists in redefine/specs.txt
-# Use BUILDKITE_PARALLEL_JOB, and BUILDKITE_PARALLEL_JOB_COUNT,
-# to run your current logic to select tests for the specific worker.
-filtered_spec_list_for_worker=
-
-# Pass the list of specs for the current worker to cypress.
-npx cypress run --spec ${filtered_spec_list_for_worker}
-
+# Pass the list of specs for the current worker to cypress
